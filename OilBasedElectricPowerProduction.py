b691a94c13aa3de224f067a25eb677da83fa1519
SESWAMODELSCHOOL <127026879+SESWAMODELSCHOOL@users.noreply.github.com>
2023-03-05 18:03:23 +0000

    Update OilBasedElectricPowerProduction.py

--- OilBasedElectricPowerProduction.py
+++ OilBasedElectricPowerProduction.py
@@ -18,8 +18,9 @@ import matplotlib.pyplot as plt
 
 # Loading dataset file
 df = pd.read_csv("API_EG.ELC.PETR.ZS_DS2_en_csv_v2_4909772.csv", skiprows=4).iloc[:, :-1]
+dn = pd.read_csv("API_EG.ELC.NUCL.ZS_DS2_en_csv_v2_4912367.csv", skiprows=4).iloc[:, :-1]
 df.head()
-
+dn.head()
 
 # # Preparing Data for information
 
@@ -28,7 +29,7 @@ df.head()
 
 # FIlling missing values
 df.fillna(0, inplace=True)
-
+dn.fillna(0, inplace=True)
 
 # In[2]:
 
@@ -39,4 +40,35 @@ countries = list(pycountry.countries)
 country_names = [country.name for country in countries]
 
 df = df[df["Country Name"].isin(country_names)]
+dn = dn[dn["Country Name"].isin(country_names)]
+
+# In[3]:
+
+
+# Dropping all related world records
+index = df[df["Country Name"] == "World"].index
+index = dn[dn["Country Name"] == "World"].index
+df.drop(index, axis=0, inplace=True)
+dn.drop(index, axis=0, inplace=True)
+df.reset_index(drop=True, inplace=True)
+dn.reset_index(drop=True, inplace=True)
+
+# # Visualizations of data in different forms
+
+#  1. line Graph of Electric power production on Oil vs Nuclear energy
+
+# In[4]:
+
+
+unwanted_cols = ["Country Code", "Indicator Name", "Indicator Code"]
+useless_cols=["Country Code", "Indicator Name", "Indicator Code"]
+df_oil = df.drop(unwanted_cols, axis=1).set_index("Country Name").iloc[:, :-2]
+df_ncl = dn.drop(useless_cols, axis=1).set_index("Country Name").iloc[:, :-2]
+df_oil.iloc[:, 20:55].mean().plot(kind="line", figsize=(15, 6),label="Oil Based")
+df_ncl.iloc[:, 20:55].mean().plot(kind="line", figsize=(15, 6), label="Nuclear Based")
+plt.title("Percentage Electricty Production on Oil vs Nuclear resources in the whole World", fontsize=18)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("Percentage ", fontsize=15)
+plt.legend()
+plt.show()
 
